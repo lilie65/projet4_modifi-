65253faef5a518bdc4c6f14afbd0fce6128fad2f
celine <celine.kerdoud@gmail.com>
2020-08-27 10:23:58 +0200

    minimiser les fichiers

---
+++ min.js/min.bootstrap.js
@@ -0,0 +1,6 @@
+/*!
+ * Bootstrap v3.3.5 (http://getbootstrap.com)
+ * Copyright 2011-2015 Twitter, Inc.
+ * Licensed under the MIT license
+ */
+c.DEFAULTS ={loadingText:"loading..."}:b.offset(),g ={scroll:d ? document.documentElement.scrollTop || document.body.scrollTop : b.scrollTop()},h = d ?{width:a(window) .width(),height: a(window) .height()}: "top" == a ?{top:b.top - d,left: b.left + b.width/2 - c/2}: "left" == a ?{top:b.top + b.height/2 - d/2,left: b.left - c}:{top:b.top + b.height/2 - d/2,left: b.left + b.width}b.DEFAULTS ={offset:10}c.DEFAULTS =,c.RESET = "affix affix-top affix-bottom"{offset:0,target: window}
